eac6fd0ae9a1387e0ed4a0313df4cefff732ceb8
Chris Fadden <cfadden3@localhost.localdomain>
2015-07-28 19:58:50 -0500

    Added comments, and restructured code a bit.  No significant changes

--- rank.py
+++ rank.py
@@ -2,30 +2,67 @@ import operator
 import math
 from datetime import date
 
-f = open('wagstatscfb2014.csv', 'r')
-games = []
-headers = [x.strip() for x in f.readline().split(',')]
-headers = headers[2:18]
-line = f.readline()
+#********************
+# Constants
+#********************
 start = date(2014, 8, 26)
 weeks = 14.0
+statsPerTeam = 16
+numRankings = 30
+f = open('wagstatscfb2014.csv', 'r')
+
+#********************
+# Functions
+#********************
+def getOutcome(game):
+	home = game["Home Team"]
+	vis = game["Vis Team"]
+	homeTeam = home["Team"]
+	visTeam = vis["Team"]
+	homeScore = int(home["Score"])
+	visScore = int(vis["Score"])
+	diff = homeScore - visScore
+	if diff > 0:
+		return (homeTeam, visTeam, diff)
+	else:
+		return (visTeam, homeTeam, -diff)
+
+#**************************
+# Initialize arrays
+#**************************
+games = [] #list of maps
+           #keys = Date, age, Vis Team, Home Team
+           #what's the point of age?
+teamScores = {}
+
+
+#********************************
+# Eventually in a function call:  ParseInput("file.csv"):
+#********************************
+headers = [x.strip() for x in f.readline().split(',')]
+headers = headers[2:2+statsPerTeam]
+line = f.readline()
+
 while len(line) > 0:
 	line = line.rstrip().split(',')
 	day = line[0].strip()
-	dateA = [int(x) for x in day.split("-")]
+	
+        #Whats the point of age, playoffs? 
+        dateA = [int(x) for x in day.split("-")]
 	numDate = date(dateA[0], dateA[1], dateA[2])
 	diff = (numDate-start).days
 	age = (diff//7) + 1
-	game = {"Date": date, "Age": age, "Vis Team": {"Team": line[1].strip()}, "Home Team": {"Team": line[18].strip()}}
+	
+        
+        game = {"Date": date, "Age": age, "Vis Team": {"Team": line[1].strip()}, "Home Team": {"Team": line[18].strip()}}
 	for i in range(len(headers)):
 		game["Vis Team"][headers[i]] = line[i+2].strip()
-	for j in range(len(headers)):
-		game["Home Team"][headers[j]] = line[j+19].strip()
+	#for j in range(len(headers))
+		game["Home Team"][headers[i]] = line[i+19].strip()
 	line = f.readline()
 	games.append(game)
-f.close()
 
-teamScores = {}
+f.close()
 
 for game in games:
 	age = game["Age"]
@@ -64,23 +101,10 @@ for game in games:
 	else:
 		teamScores[visTeam] = visTotal
 
-def getOutcome(game):
-	home = game["Home Team"]
-	vis = game["Vis Team"]
-	homeTeam = home["Team"]
-	visTeam = vis["Team"]
-	homeScore = int(home["Score"])
-	visScore = int(vis["Score"])
-	diff = homeScore - visScore
-	if diff > 0:
-		return (homeTeam, visTeam, diff)
-	else:
-		return (visTeam, homeTeam, -diff)
-
 sorted_x = sorted(teamScores.items(), key=operator.itemgetter(1), reverse=True)
 numTeams = len(sorted_x) + 1
 
-for i in range(100):
+for i in range(100): #where does 100 come from?
 	sorted_y = [a for (a, _) in sorted_x]
 
 	for game in games:
@@ -95,7 +119,7 @@ for i in range(100):
 		teamScores[result[1]] -= (place1 ** 2) * age
 	sorted_x = sorted(teamScores.items(), key=operator.itemgetter(1), reverse=True)
 
-for tup in range(30):
+for tup in range(numRankings):
 	string = "{}: {}".format(tup+1, sorted_x[tup][0])
 	print(string)
 #input()
