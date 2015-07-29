f11a50400e1d45d65941de19266a5166cd50eba8
Chris Fadden <cfadden3@localhost.localdomain>
2015-07-29 14:42:23 -0500

    Refactored into module and function design

--- rank.py
+++ rank.py
@@ -1,6 +1,7 @@
 import operator
 import math
 from datetime import date
+from rankModule import getOutcome, ParseInput
 
 #********************
 # Constants
@@ -9,60 +10,18 @@ start = date(2014, 8, 26)
 weeks = 14.0
 statsPerTeam = 16
 numRankings = 30
+numIterations = 100
 f = open('wagstatscfb2014.csv', 'r')
-
-#********************
-# Functions
-#********************
-def getOutcome(game):
-	home = game["Home Team"]
-	vis = game["Vis Team"]
-	homeTeam = home["Team"]
-	visTeam = vis["Team"]
-	homeScore = int(home["Score"])
-	visScore = int(vis["Score"])
-	diff = homeScore - visScore
-	if diff > 0:
-		return (homeTeam, visTeam, diff)
-	else:
-		return (visTeam, homeTeam, -diff)
 
 #**************************
 # Initialize arrays
 #**************************
-games = [] #list of maps
-           #keys = Date, age, Vis Team, Home Team
-           #what's the point of age?
+
 teamScores = {}
 
-
-#********************************
-# Eventually in a function call:  ParseInput("file.csv"):
-#********************************
-headers = [x.strip() for x in f.readline().split(',')]
-headers = headers[2:2+statsPerTeam]
-line = f.readline()
-
-while len(line) > 0:
-	line = line.rstrip().split(',')
-	day = line[0].strip()
-	
-        #Whats the point of age, playoffs? 
-        dateA = [int(x) for x in day.split("-")]
-	numDate = date(dateA[0], dateA[1], dateA[2])
-	diff = (numDate-start).days
-	age = (diff//7) + 1
-	
-        
-        game = {"Date": date, "Age": age, "Vis Team": {"Team": line[1].strip()}, "Home Team": {"Team": line[18].strip()}}
-	for i in range(len(headers)):
-		game["Vis Team"][headers[i]] = line[i+2].strip()
-	#for j in range(len(headers))
-		game["Home Team"][headers[i]] = line[i+19].strip()
-	line = f.readline()
-	games.append(game)
-
-f.close()
+games = ParseInput(f, statsPerTeam) #list of maps
+                                    #keys = Date, age, Vis Team, Home Team
+                                    #age = weighting to make more recent games count more
 
 for game in games:
 	age = game["Age"]
@@ -84,7 +43,9 @@ for game in games:
 	diff = homeScore - visScore;
 	homeTotal += diff*2 * age;
 	visTotal -= diff*2 * age;
-	if diff > 0:
+	
+        #Home team win counts less than visiting team win 
+        if diff > 0:
 		homeTotal += 200 * age * winMult
 		visTotal -= 150 * age * loseMult
 	else:
@@ -104,7 +65,7 @@ for game in games:
 sorted_x = sorted(teamScores.items(), key=operator.itemgetter(1), reverse=True)
 numTeams = len(sorted_x) + 1
 
-for i in range(100): #where does 100 come from?
+for i in range(numIterations):
 	sorted_y = [a for (a, _) in sorted_x]
 
 	for game in games:
@@ -122,4 +83,4 @@ for i in range(100): #where does 100 come from?
 for tup in range(numRankings):
 	string = "{}: {}".format(tup+1, sorted_x[tup][0])
 	print(string)
-#input()
+
